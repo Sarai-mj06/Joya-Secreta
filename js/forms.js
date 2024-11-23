a9b4f113bbf2da41b536379710e9e48fb88262cb
Sarai-mj06 <jaramillosarai13@gmail.com>
2024-11-22 20:12:07 -0500

    Responsividad y cambios en ciertos documentos

--- js/forms.js
+++ js/forms.js
@@ -1,60 +1,67 @@
 // Muestra o desaparece el cuadro de logueo
 function mostrarLogin() {
-    document.getElementById('formulario-registro').classList.add('oculto');
-    document.getElementById('formulario-login').classList.remove('oculto');
+  document.getElementById("formulario-registro").classList.add("oculto");
+  document.getElementById("formulario-login").classList.remove("oculto");
 }
 
 // Muestra o desaparece el cuadro del registro
 function mostrarRegistro() {
-    document.getElementById('formulario-login').classList.add('oculto');
-    document.getElementById('formulario-registro').classList.remove('oculto');
+  document.getElementById("formulario-login").classList.add("oculto");
+  document.getElementById("formulario-registro").classList.remove("oculto");
 }
 
+// Almacena los datos que se pongan en el registro
+document
+  .getElementById("registro-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-// Almacena los datos que se pongan en el registro 
-document.getElementById('registro-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-
-    const nombre = document.getElementById('nombre').value;
-    const apellido = document.getElementById('apellido').value;
-    const correo = document.getElementById('correo').value;
-    const contrasena = document.getElementById('contrasena').value;
-    const genero = document.getElementById('genero').value;
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const correo = document.getElementById("correo").value;
+    const contrasena = document.getElementById("contrasena").value;
+    const genero = document.getElementById("genero").value;
 
     const usuario = {
-        nombre: nombre,
-        apellido: apellido,
-        correo: correo,
-        contrasena: contrasena,
-        genero: genero
+      nombre: nombre,
+      apellido: apellido,
+      correo: correo,
+      contrasena: contrasena,
+      genero: genero,
     };
 
-    localStorage.setItem('usuario', JSON.stringify(usuario));
+    localStorage.setItem("usuario", JSON.stringify(usuario));
 
     this.reset();
 
     mostrarLogin();
-    
-    alert('Registro exitoso. Por favor, inicia sesión.');
-});
+
+    alert("Registro exitoso. Por favor, inicia sesión.");
+  });
 
 // Verifica que los datos del loguin sean los mismo que los del registro
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    const correoLogin = document.getElementById('correo-login').value;
-    const contrasenaLogin = document.getElementById('contrasena-login').value;
+    const correoLogin = document.getElementById("correo-login").value;
+    const contrasenaLogin = document.getElementById("contrasena-login").value;
 
-    const usuarioGuardado = JSON.parse(localStorage.getItem('usuario'));
+    const usuarioGuardado = JSON.parse(localStorage.getItem("usuario"));
 
     if (usuarioGuardado) {
-        if (usuarioGuardado.correo === correoLogin && usuarioGuardado.contrasena === contrasenaLogin) {
-
-            window.location.href = 'pages/principal.html';
-        } else {
-            alert('Credenciales incorrectas. Por favor, intenta de nuevo.');
-        }
+      if (
+        usuarioGuardado.correo === correoLogin &&
+        usuarioGuardado.contrasena === contrasenaLogin
+      ) {
+        window.location.href = "pages/principal.html";
+      } else {
+        alert("Credenciales incorrectas. Por favor, intenta de nuevo.");
+      }
     } else {
-        alert('No hay un usuario registrado con ese correo. Por favor, regístrate primero.');
+      alert(
+        "No hay un usuario registrado con ese correo. Por favor, regístrate primero."
+      );
     }
-});
+  });
--- js/mostrarNombre.js
+++ js/mostrarNombre.js
@@ -1,10 +1,11 @@
-const usuarioGuardado = JSON.parse(localStorage.getItem('usuario'));
+const usuarioGuardado = JSON.parse(localStorage.getItem("usuario"));
 
 // Verificar si hay un usuario guardado
 if (usuarioGuardado) {
-    // Mostrar el nombre del usuario en el span
-    document.getElementById('nombre-usuario').textContent = usuarioGuardado.nombre;
+  // Mostrar el nombre del usuario en el span
+  document.getElementById("nombre-usuario").textContent =
+    usuarioGuardado.nombre;
 } else {
-    // Redirigir a la página de registro si no hay usuario guardado
-    window.location.href = 'registro.html'; // Cambia esto por la URL de tu página de registro
+  // Redirigir a la página de registro si no hay usuario guardado
+  window.location.href = "registro.html"; // Cambia esto por la URL de tu página de registro
 }
